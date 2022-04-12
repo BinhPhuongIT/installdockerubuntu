30887f75126dfe87ba8577f0ad1880f93f57e1af
Lê Bình Phương <69758987+BinhPhuongIT@users.noreply.github.com>
2022-04-12 15:16:29 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@ sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt update
+sudo apt update -y
 apt-cache policy docker-ce
-sudo apt install docker-ce
+sudo apt install docker-ce -y
 sudo systemctl status docker
 
-sudo apt install docker-compose
+sudo apt install docker-compose -y
