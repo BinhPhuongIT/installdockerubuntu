417ce1f44aac5660d8c14efb69d692676aea6080
Lê Bình Phương <69758987+BinhPhuongIT@users.noreply.github.com>
2022-04-26 14:11:24 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,12 +27,3 @@ sudo apt install docker-compose -y
 
 echo "Add docker user"
 sudo usermod -aG docker $USER
-
-# Uninstall Docker Engine
-# Uninstall the Docker Engine, CLI, and Containerd packages:
-
-# sudo apt-get purge docker-ce docker-ce-cli containerd.io
-# Images, containers, volumes, or customized configuration files on your host are not automatically removed. To delete all images, containers, and volumes:
-
-# sudo rm -rf /var/lib/docker
-# sudo rm -rf /var/lib/containerd
