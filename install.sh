de66b351a5f8ee24b6a59228d56175dd808d128c
Lê Bình Phương <69758987+BinhPhuongIT@users.noreply.github.com>
2022-04-26 13:59:04 +0700

    update comment

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
+echo "Updating repo"
 sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
+
+echo "Adding key docker"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+echo "Add repository docker"
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update -y
 apt-cache policy docker-ce
+
+echo "install Docker CE"
 sudo apt install docker-ce -y
 
-#sudo systemctl status docker
-
+echo "Install Docker compose"
 sudo apt install docker-compose -y
